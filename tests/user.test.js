d8a8b688b180c535d933c58e6e8ab924fa87f8f0
Francisco Gameiro <gameirofrancisco6@gmail.com>
2024-05-29 12:39:38 +0100

    Testes de criação de conta.

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,11 +1,12 @@
 const axios = require('axios');
 
 const API_BASE_URL = 'http://127.0.0.1:3000'
+const JWT_TOKEN = ''
 
 test('Criar conta com todos os dados preenchidos', async () => {
     const response = await axios({
         method: 'post',
-        url: `http://127.0.0.1:3000/users`,
+        url: `${API_BASE_URL}/users`,
         data: {
             "name": "John Doe",
             "username": "j0hnDo3",
@@ -17,6 +18,52 @@ test('Criar conta com todos os dados preenchidos', async () => {
       });
     console.log(response);
     expect(response.status).toBe(201)
+})
+
+test('Criar conta com dados não requiridos por preencher', async () => {
+    const response = await axios({
+        method: 'post',
+        url: `${API_BASE_URL}/users`,
+        data: {
+            "name": "Jane Doe",
+            "username": "j4neDo3",
+            "email": "janeDoe@email.com",
+            "password": "Not4Pa55",
+        }
+      });
+    console.log(response);
+    expect(response.status).toBe(201)
+})
+
+test('Criar conta com dados requiridos por preencher', async () => {
+    const response = await axios({
+        method: 'post',
+        url: `${API_BASE_URL}/users`,
+        data: {
+            "name": "Johnny Appleseed",
+            "username": "appleseed_john",
+            "password": "Not4Pa55",
+        }
+      });
+    console.log(response);
+    expect(response.status).toBe(400)
+})
+
+test('Criar conta com dados repetidos', async () => {
+    const response = await axios({
+        method: 'post',
+        url: `${API_BASE_URL}/users`,
+        data: {
+            "name": "John Doe",
+            "username": "john_doe_2004",
+            "email": "johnDoe@email.com",
+            "password": "Not4Pa55",
+            "address": "Rua do Bairro",
+            "nationality": "EN"
+        }
+      });
+    console.log(response);
+    expect(response.status).toBe(401)
 })
 
 //? CONTINUAR
