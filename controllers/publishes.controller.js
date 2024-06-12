38f7062c595bdf44db000798523cb2fd0f887f8d
Francisco Gameiro <gameirofrancisco6@gmail.com>
2024-06-12 09:01:42 +0100

    Implementação de funcionalidades de adicionar e apagar publicações

--- controllers/publishes.controller.js
+++ controllers/publishes.controller.js
@@ -1,21 +1,78 @@
 // import users data
-let publishes = require("../models/publishes.model");
+const db = require("../models/index.js");
+let publications = db.publications
+let comment = db.comment
+const { clear } = require("console");
+const { Op, ValidationError, where, JSON } = require("sequelize");
+const { ErrorHandler } = require("../utils/error.js");
+const { title } = require("process");
 
 //-----------------------------------------------//
 //-------------Comandos apiRest------------------//
 //-----------------------------------------------//
 
 //GET/publishes | Obs:Content-Length: false
-exports.findAll = (req, res) => {
-  console.log("findAll");//console.table(publishes)
-  res.json(publishes);
+exports.findAll = async (req, res, next) => {
+  try {
+    
+  } catch (error) {
+    
+  }
 };
 
+exports.createPublication = async (req, res, next) => {
+  try {
+    if (!req.body.title || !req.body.body){
+      throw new ErrorHandler(400, 'This post does not have any content.')
+    }
+
+    let publication = await publications.create({
+      UserId: req.loggedUserId,
+      title: req.body.title,
+      body: req.body.body
+    })
+
+    res.status(200).json({message: 'The publication was succesfully posted!'})
+  } catch (error) {
+    next(error)
+  }
+}
 
 
+exports.deletePublication = async (req, res, next) => {
+  try {
+    if(!req.params.id){
+      throw new ErrorHandler(400, 'The id was not submitted')
+    }
+    if (isNaN(req.params.id) || Number.isInteger(req.params.id)) {
+      throw new ErrorHandler(400, 'The id is not a lavid type')
+    }
+
+    let publication = await publications.findOne({
+      attributes: ['id', 'UserId'],
+      where: {
+        id: req.params.id
+      }
+    })
+
+    if (publication == null){
+      throw new ErrorHandler(404, 'Publication not found')
+    }
+
+    if (!(publication.UserId == req.loggedUserId) && req.loggedUserType != 'admin'){
+      throw new ErrorHandler(403, 'You must had published the post or being an admin to delete it.')
+    };
+
+    publication.destroy()
+    return res.status(200).send({message: 'Publication successfully deleted'})
+    
+  } catch (error) {
+    next(error)
+  }
+}
 
 //Funções de apoio
-exports.bodyValidator = (req, res, next) => {
+exports.bodyValidator = async (req, res, next) => {
   if(!isRegistered(req) && req.method=='POST') {
     console.log("POST")
     next();
--- routes/publishes.routes.js
+++ routes/publishes.routes.js
@@ -3,14 +3,25 @@ const router = express.Router();
 
 // import controller middleware
 const publishesController = require("../controllers/publishes.controller");
+const authController = require("../controllers/auth.controller")
 
 /*----------------Users----------------*/
 
 // Rota ('/'))
 router.route('/')
 .get(publishesController.findAll)
+.post(authController.verifyToken, publishesController.createPublication)
 
+ router.route('/:id')
+//.get(publishesController.findOne)
+.delete(authController.verifyToken, publishesController.deletePublication)
 
+/* router.route('/:id/comments')
+.get(publishesController.getComments)
+.post(authController.verifyToken, publishesController.addComment) */
+
+/* router.route('/:id/comments/:commentId')
+.delete(authController.verifyToken, publishesController.deleteComment) */
 
 router.all('*', (req, res) => {
 res.status(404).json({ message: 'Error' }); //Mensagem genérica
