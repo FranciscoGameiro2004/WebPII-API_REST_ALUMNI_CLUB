dc4fca67131f15806ef83d3cbdff06c7d376e874
Francisco Gameiro <gameirofrancisco6@gmail.com>
2024-06-12 09:32:19 +0100

    Implementação da funcionalidade de pesquisa de publicações

--- controllers/publishes.controller.js
+++ controllers/publishes.controller.js
@@ -14,9 +14,82 @@ const { title } = require("process");
 //GET/publishes | Obs:Content-Length: false
 exports.findAll = async (req, res, next) => {
   try {
-    
+    const currentPage = req.query.page >= 0 ? req.query.page : 0;
+    const limit = +req.query.limit;
+
+    if (limit < 5 || !Number.isInteger(limit)) {
+      throw new ErrorHandler(
+        400,
+        "Limit must be a positive integer, greater than 5"
+      );
+    }
+
+    let publicationsList = await publications.findAndCountAll({
+      attributes: ["id", "UserId", "title"],
+      raw: true,
+      limit: limit,
+      offset: currentPage ? currentPage * limit : 0,
+      where: {
+         title: { [Op.like]: `%${req.query.search != undefined ? req.query.search : ''}%` },
+        /* [Op.or]: 
+        [
+          { title: { [Op.like]: `%${req.query.search != undefined ? req.query.search : ''}%` } },
+        ], */
+      },
+      include: [{model: db.users, attributes: ['username']}]
+    });
+
+    publicationsList.rows.forEach((publication, index) => {
+      publicationsList.rows[index].links = [
+        { rel: "self", href: `/publishes/${publication.id}`, method: "GET" },
+      ];
+
+      let userTarget = db.users.findOne({
+        attributes: ['username'],
+        raw: true,
+        where: {
+          id: publication.UserId
+        }
+      })
+      publicationsList.rows[index].user = userTarget.username
+      delete publicationsList.rows[index].UserId
+    });
+
+    if (publicationsList.rows.length < 1) {
+      throw new ErrorHandler(404, "Page not found");
+    }
+
+    const numPages = Math.ceil(publicationsList.length / limit);
+
+    links = [];
+
+    if (currentPage > 0) {
+      links.push({
+        rel: "next-page",
+        href: `/publishes?limit=${limit}&page=${currentPage - 1}`,
+        method: "GET",
+      });
+    }
+    if (currentPage < limit) {
+      links.push({
+        rel: "next-page",
+        href: `/publishes?limit=${limit}&page=${currentPage + 1}`,
+        method: "GET",
+      });
+    }
+
+    res.status(200).json({
+      pagination: {
+        total: publicationsList.rows.length,
+        pages: numPages,
+        current: currentPage,
+        limit: limit,
+      },
+      data: publicationsList.rows,
+      links: links,
+    });
   } catch (error) {
-    
+    next(error)
   }
 };
 
